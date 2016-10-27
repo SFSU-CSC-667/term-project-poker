40b81f8afb36ac1075f984f14f7efad34fc6ad78
tonytran1 <tonylyfe@gmail.com>
2016-10-26 20:34:34 -0700

    Updated Schema to match current (WIP)

--- db/Schema.sql
+++ db/Schema.sql
@@ -8,20 +8,36 @@ CREATE TABLE Users (
   Avatar INTEGER
 );
 
-CREATE TABLE GameList
-(
+CREATE TABLE Games (
   GameId SERIAL PRIMARY KEY,
   MaxPlayers INTEGER,
   MinChips INTEGER,
-  SeatsAvailable INTEGER
+  SeatsTaken INTEGER,
+  Turn INTEGER
 );
 
-CREATE TABLE GameTables
-(
+CREATE TABLE Players (
   GameId INTEGER REFERENCES GameList(GameId),
   UserId INTEGER REFERENCES Users(UserId),
   Bid INTEGER,
-  Chips INTEGER,
-  Cards VARCHAR(255),
-  Status INTEGER
+  BuyIn INTEGER,
+  NetGain INTEGER,
+  IsPlaying BOOLEAN,
+  SeatNumber INTEGER
+);
+
+CREATE TABLE Deck (
+  CardId SERIAL PRIMARY KEY,
+  Card VARCHAR(255),
+  Image VARCHAR(255)
+);
+
+CREATE TABLE PlayersCards (
+  GameId INTEGER REFERENCES GameList(GameId),
+  UserId INTEGER REFERENCES Users(UserId),
+  FlopCard1 INTEGER REFERENCES Deck(CardId),
+  FlopCard2 INTEGER REFERENCES Deck(CardId),
+  FlopCard3 INTEGER REFERENCES Deck(CardId),
+  TurnCard INTEGER REFERENCES Deck(CardId),
+  RiverCard INTEGER REFERENCES Deck(CardId),
 );
