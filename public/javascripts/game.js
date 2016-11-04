f9b62381bc042ae6264c20855d806d15ac3f02da
tony tran <tonylyfe@gmail.com>
2016-11-04 03:45:26 -0700

    Implemented card dealing
    Currently only works with two players
    
    Basic UI for card dealing working for two players in gameroom

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -11,7 +11,7 @@
   socket.emit('game viewer');
 
   socket.on('new player', data => {
-    $("#" + data.seat).html(data.html);
+    $(`#${ data.seat }`).html(data.html);
     seatsOccupied.push(data.seat);
   });
 
@@ -19,13 +19,24 @@
     gameLoop(data.turn);
   });
 
-  socket.on('get cards', data => {
-    $("#" + data.seat).append(cardImages(data.first, data.second));
+  socket.on('player cards', data => {
+    $(`#${ data.seat }-cards`).html(cardImages(data.first, data.second));
     seatsOccupied.forEach((seat) => {
-      if (seat === data.seat)
-        return;
-      $("#" + seat).append(cardImages('face-down', 'face-down'));
-    })
+      if (seat !== data.seat)
+        $(`#${ seat }-cards`).html(cardImages('face-down', 'face-down'));
+    });
+  });
+
+  socket.on('draw flop cards', data => {
+    $("#dealer-cards").html(cardImages('face-down', data.first, data.second, data.third));
+  });
+
+  socket.on('draw turn card', data => {
+    $("#dealer-cards").append(cardImages(data.turnCard));
+  })
+
+  socket.on('draw river card', data => {
+    $("#dealer-cards").append(cardImages(data.riverCard));
   });
 
   function cardImages(...cardNames) {
@@ -37,11 +48,11 @@
   }
 
   function gameLoop(turn) {
-    $("#" + seatsOccupied[turn]).append("<button class='next-btn btn'>Next</button>");
+    $(`#${ seatsOccupied[turn] }-cards`).append("<button class='next-btn btn btn-success'>Next</button>");
     $(".next-btn").on('click', event => {
       event.preventDefault();
-      $("#" + seatsOccupied[turn]).children('button').remove();
-      socket.emit('next button', { gameId: 1 });
+      $(`#${ seatsOccupied[turn] }-cards`).children('button').remove();
+      socket.emit('next button', { gameId: 1, turn: turn });
     });
   }
 })();
--- server/gameEvents.js
+++ server/gameEvents.js
@@ -1,6 +1,4 @@
-let gameStarted = false,
-turn,
-deck = {};
+let game = {};
 
 const gameEvents = (io, socket, players) => {
   const Deck = require('../poker-game/deck.js');
@@ -9,51 +7,101 @@ const gameEvents = (io, socket, players) => {
     socket.join('game room');
   });
 
+  socket.on('next button', (data) => {
+    let Game = game[data.gameId];
+    if (players[Game.turn + 1]) {
+      Game.turn++;
+      players[data.gameId][Game.turn].emit('run game', { turn: Game.turn });
+    } else {
+      checkRound(Game.round, data);
+      console.log("Round over")
+      Game.round++;
+      Game.turn = 0;
+      players[data.gameId][Game.turn].emit('run game', { turn: Game.turn });
+    }
+  });
+
   socket.on('join request', (data) => {
     if (!players[data.gameId])
       players[data.gameId] = [];
     if (players[data.gameId].indexOf(socket) > -1)
       return;
+    if (!game[data.gameId])
+      game[data.gameId] = { deck: new Deck(), turn: 0, round: 0, gameStarted: false }
+    acceptRequest(socket, data);
+  });
+
+  function acceptRequest(socket, data) {
     players[data.gameId].push(socket);
     socket['seat'] = data.seat;
-    console.log(players[data.gameId].indexOf(socket));
     io.to('game room').emit('new player', {
       seat: data.seat,
       html: "<p>Name: " + data.user + " " + players[data.gameId].indexOf(socket) + "</p>"
     });
     console.log("Players " + players[data.gameId].length);
-    if (players[data.gameId].length > 1 && !gameStarted) {
+    if (players[data.gameId].length > 1 && !game[data.gameId].gameStarted) {
       startGame(data);
     }
-  });
-
-  socket.on('next button', (data) => {
-    if (players[turn + 1]) {
-      turn++;
-      players[data.gameId][turn].emit('run game', { turn: turn });
-    } else {
-      // Round over
-      console.log("Round over");
-      turn = 0;
-      players[data.gameId][turn].emit('run game', { turn: turn });
-    }
-  });
+  }
 
   function startGame(data) {
-    deck[data.gameId] = new Deck();
-    deck[data.gameId].shuffle();
-    turn = 0;
-    gameStarted = true;
-    // Testing stuff
+    game[data.gameId].deck.shuffle();
+    drawUserCards(data);
+    drawFlopCards(data);
+    players[data.gameId][game[data.gameId].turn].emit('run game', { turn: game[data.gameId].turn });
+  }
+
+  function checkRound(round, data) {
+    let Game = game[data.gameId];
+    switch (round) {
+      case 0:
+        if (!Game.turnCardDelt && data.turn === players[data.gameId].length - 1) {
+          drawTurnCard(data);
+          turnCardsDelt = 1;
+        }
+        break;
+      case 1:
+        if (!Game.riverCardDelt && data.turn === players[data.gameId].length - 1) {
+          drawRiverCard(data);
+          riverCardDelt = 1;
+        }
+        break;
+      case 2:
+        startGame(data);
+        return 1;
+    }
+  }
+  function drawFlopCards(data) {
+    io.to('game room').emit('draw flop cards', {
+      first: game[data.gameId].deck.draw(),
+      second: game[data.gameId].deck.draw(),
+      third: game[data.gameId].deck.draw()
+    });
+  }
+
+  function drawUserCards(data) {
     players[data.gameId].forEach((player) => {
-      player.emit('get cards', {
+      player.emit('player cards', {
         seat: player['seat'],
-        first: deck[data.gameId].draw(),
-        second: deck[data.gameId].draw()
+        first: game[data.gameId].deck.draw(),
+        second: game[data.gameId].deck.draw()
       });
     });
-    players[data.gameId][turn].emit('run game', { turn: turn });
   }
+
+  function drawTurnCard(data) {
+    io.to('game room').emit('draw turn card', {
+      turnCard: game[data.gameId].deck.draw()
+    });
+  }
+
+  function drawRiverCard(data) {
+    io.to('game room').emit('draw river card', {
+      riverCard: game[data.gameId].deck.draw()
+    });
+  }
+
 };
+
 
 module.exports = gameEvents;
