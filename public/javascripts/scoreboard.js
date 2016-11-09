0fdd9f9fdbc6f26de5cdc5d03dbbb2986abd7991
ivan <ph2622@gmail.com>
2016-11-08 18:17:25 -0800

    Fixed scoreboard for client.

--- public/javascripts/scoreboard.js
+++ public/javascripts/scoreboard.js
@@ -1,9 +1,20 @@
 (() => {
-    socket.emit('update scoreboard');
+
+    setInterval(refreshScoreboard => {
+        socket.emit('update scoreboard');
+    }, 5000);
+
     socket.on('score update', data => {
-        $('.scoreboard').clear();
-        for (var score in data) {
-            $('.scoreboard').append('<li>' + score['firstname'] + " " + score['lastname'] + " " + score['chips'] + '</li>');
+        $('.scoreboard').empty();
+        $('.scoreboard').append('<li><span class="col-lg-4">Rank</span>' +
+            '<span class="col-lg-4">Name</span>' +
+            '<span class="col-lg-4">Score</span></span></li>');
+        var rank = 1;
+        for (var key in data) {
+            $('.scoreboard').append('<li><span class="col-lg-4">' + rank + '</span>' +
+                '<span class="col-lg-4">' + data[key]["firstname"] + data[key]["lastname"] + '</span>' +
+                '<span class="col-lg-4">' + data[key]["chips"] + '</span></li>');
+            rank++;
         }
     });
 })();
