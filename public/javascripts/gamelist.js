b102b4cf7d5bde8615a270523dbaf8b1502b377f
ivan <ph2622@gmail.com>
2016-11-19 12:56:13 -0800

    Changed ul list to span. Updated from master codes.

--- public/javascripts/gamelist.js
+++ public/javascripts/gamelist.js
@@ -8,18 +8,18 @@
 
     socket.on('gamelist update', data => {
         $('.gamelist').empty();
-        $('.gamelist').append('<li><span class="col-lg-1">No.</span>' +
+        $('.gamelist').append('<span><span class="col-lg-1">No.</span>' +
             '<span class="col-lg-4">Name</span>' +
             '<span class="col-lg-1">Maximum Player</span>' +
             '<span class="col-lg-2">Stake</span>' +
-            '<span class="col-lg-2">BuyIn</span></li>');
+            '<span class="col-lg-2">BuyIn</span></span>');
         for (var key in data) {
-            $('.gamelist').append('<li><span class="col-lg-1">' + data[key]["gameid"] + '</span>' +
+            $('.gamelist').append('<span><span class="col-lg-1">' + data[key]["gameid"] + '</span>' +
                 '<span class="col-lg-4">' + data[key]["gamename"] + '</span>' +
                 '<span class="col-lg-1">' + data[key]["maxplayers"] + '</span>' +
                 '<span class="col-lg-2">' + data[key]["minbid"] + '</span>' +
                 '<span class="col-lg-2">' + data[key]["minchips"] + '</span>' +
-                '<button class="btn btn-primary" onclick="viewGame(' + data[key]["gameid"] + ')">View</button></li>');
+                '<button class="btn btn-primary" onclick="viewGame(' + data[key]["gameid"] + ')">View</button></span>');
         }
     });
 })();
--- public/javascripts/scoreboard.js
+++ public/javascripts/scoreboard.js
@@ -6,14 +6,14 @@
 
     socket.on('score update', data => {
         $('.scoreboard').empty();
-        $('.scoreboard').append('<li><span class="col-lg-4">Rank</span>' +
+        $('.scoreboard').append('<span><span class="col-lg-4">Rank</span>' +
             '<span class="col-lg-4">Name</span>' +
-            '<span class="col-lg-4">Score</span></span></li>');
+            '<span class="col-lg-4">Score</span></span></span>');
         var rank = 1;
         for (var key in data) {
-            $('.scoreboard').append('<li><span class="col-lg-4">' + rank + '</span>' +
+            $('.scoreboard').append('<span><span class="col-lg-4">' + rank + '</span>' +
                 '<span class="col-lg-4">' + data[key]["firstname"] + data[key]["lastname"] + '</span>' +
-                '<span class="col-lg-4">' + data[key]["chips"] + '</span></li>');
+                '<span class="col-lg-4">' + data[key]["chips"] + '</span></span>');
             rank++;
         }
     });
