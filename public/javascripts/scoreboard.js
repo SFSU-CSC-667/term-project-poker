90c2e99b138d846a55cc8504d26e246bc2b26740
Camille David <mcdavid@mail.sfsu.edu>
2016-12-10 19:59:46 -0800

    More UI Changes - modified slider style - added margins to action buttons - added wins to scoreboard

--- public/javascripts/scoreboard.js
+++ public/javascripts/scoreboard.js
@@ -6,11 +6,11 @@
 
     socket.on('score update', data => {
         $('.scoreboard').empty();
-        $('.scoreboard').append('<tr><th>Rank</th><th>Name</th><th>Score</th></tr>');
+        $('.scoreboard').append('<tr><th>Rank</th><th>Name</th><th>Score</th><th>Wins</th><</tr>');
         var rank = 1;
         for (var key in data) {
-            $('.scoreboard').append('<tr><td style="width: 20%;">' + rank + '</td><td style="width: 80%;">' + data[key]["firstname"] + " " + data[key]["lastname"] +
-                '</td><td style="width: 20%;">' + data[key]["chips"] + '</td></tr>');
+            $('.scoreboard').append('<tr><td style="width: 20%;">' + rank + '</td><td style="width: 60%;">' + data[key]["firstname"] + " " + data[key]["lastname"] +
+                '</td><td style="width: 20%;">' + data[key]["chips"] + '</td><td style="width: 20%;">' + data[key]["wins"] + '</td></tr>');
             rank++;
         }
     });
